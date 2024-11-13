b9df51b7141591b273c45f31e8403fcceded868f
MkTz <94935785+maktezi@users.noreply.github.com>
2024-11-14 07:57:58 +0800

    feat:  App.vue

---
+++ App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <button @click="toggleTheme">Toggle Theme</button>
+  </div>
+</template>
+
+<script setup>
+import { ref, watch } from 'vue'
+
+const root = document.documentElement
+const darkThemeHex = '#333'
+const lightThemeHex = '#42b983'
+const isDarkTheme = ref(false)
+
+watch(isDarkTheme, (oldValue, newValue) => {
+  const newColor = newValue ? lightThemeHex : darkThemeHex
+  root.style.setProperty('--main-color', newColor)
+})
+
+const toggleTheme = () => isDarkTheme.value = !isDarkTheme.value
+</script>
+
+<style>
+:root {
+  --main-color: #42b983;
+}
+
+body {
+  background-color: var(--main-color);
+}
+</style>
